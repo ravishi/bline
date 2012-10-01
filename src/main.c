03f983f421980605f42f2d19432a97a3c744dc29
Dirley Rodrigues <drodrigues@bambae.com.br>
2012-10-01 13:54:38 -0300

    reverting that chant code

--- src/main.c
+++ src/main.c
@@ -2,11 +2,17 @@
 #include <glib/gprintf.h>
 #include <string.h>
 #include <stdlib.h>
+#include "bline-operation-sobel.h"
 
 int
 main (gint    argc,
       gchar **argv)
 {
+  /* make sure our operation gets registered in gtype system */
+  g_type_init ();
+  
+  g_type_class_ref (BLINE_TYPE_OPERATION_SOBEL);
+
   gegl_init (&argc, &argv);
 
   {
@@ -37,7 +43,7 @@ main (gint    argc,
 
     /*< The image nodes representing operations we want to perform */
     GeglNode *display = gegl_node_create_child (gegl, "gegl:display");
-    GeglNode *edge    = gegl_node_new_child (gegl, "operation", "bline:edge-detect", NULL);
+    GeglNode *edge    = gegl_node_new_child (gegl, "operation", "bline:sobel", NULL);
     GeglNode *load    = gegl_node_new_child (gegl, "operation", "gegl:load",
                                              "path", argv[1],
                                              NULL);
